aa977bbf460fec0b0bb6c6de478e462cacd2ce66
Ziya <ziyameherremov21@gmail.com>
2024-02-20 00:27:29 +0400

    script elave olundu js/script ve kod yazildi

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+list = [
+    "alma",
+    "armut",
+    "heyva"
+
+]
+
+for (let i = 0; i < list.length; i++) {
+    const element = array[i];
+
+    if(element == array[0]){
+        console.log("Ehmed almalari artiq endirimdedir");
+    }else if(element == array[1]){
+        console.log("Armuta doyum olmaz");
+
+    }
+    else if(element == array[2]){
+        console.log("Heyva yeyin saglam qalin");
+
+    }
+}
